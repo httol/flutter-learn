266bf442c8310bcdd4a2d8d1cd26dd6c81500b4a
Tom <895652159@qq.com>
2024-04-02 21:20:49 +0800

    destructed variable

--- patterns_codelab/lib/main.dart
+++ patterns_codelab/lib/main.dart
@@ -1,19 +1,49 @@
 import 'package:flutter/material.dart';
 
+import 'data.dart';
+
 void main() {
-  runApp(const MainApp());
+  runApp(const DocumentApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class DocumentApp extends StatelessWidget {
+  const DocumentApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      theme: ThemeData(useMaterial3: true),
+      home: DocumentScreen(
+        document: Document(),
+      ),
+    );
+  }
+}
+
+class DocumentScreen extends StatelessWidget {
+  final Document document;
+
+  const DocumentScreen({
+    required this.document,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    // final metadataRecord = document.metadata;
+    final (title, :modified) = document.metadata;
+
+    return Scaffold(
+      appBar: AppBar(
+          // title: Text(metadataRecord.$1),
+          title: Text(title)),
+      body: Column(
+        children: [
+          Center(
+            // child: Text('Last Modified ${metadataRecord.modified}'),
+            child: Text('Last Modified $modified'),
+          ),
+        ],
       ),
     );
   }
